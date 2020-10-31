fe81f12f133784b8a4cc08f89c67011925748b1c
Elena Fedoseeva <horungeelena@gmail.com>
2020-10-31 15:31:39 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+        public static void main(String[] args) {
+            int price = 5640;
+            int miles = 20;
+             // делим
+            int bonus = price / miles;
+            System.out.println(bonus);
+        }
+}
